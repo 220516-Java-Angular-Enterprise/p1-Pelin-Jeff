38a17265f35a90e8199f46a4e1c6dffcf0e908aa
Jeff Sturgeon <jeff637@revature.net>
2022-06-07 12:44:55 -0700

    gitignore

---
+++ src/main/java/com/revature/ers/util/ContextLoaderListner.java
@@ -0,0 +1,33 @@
+package com.revature.ers.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.ers.daos.UserDAO;
+import com.revature.ers.services.UserService;
+import com.revature.ers.servlets.UserServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/* Need this ContextLoaderListener for our dependency injection upon deployment. */
+public class ContextLoaderListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("\nInitializing ERS web application");
+
+        /* ObjectMapper provides functionality for reading and writing JSON, either to and from basic POJOs (Plain Old Java Objects) */
+        ObjectMapper mapper = new ObjectMapper();
+
+        /* Dependency injection. */
+        UserServlet userServlet = new UserServlet(mapper, new UserService(new UserDAO()));
+
+        /* Need ServletContext class to map whatever servlet to url path. */
+        ServletContext context = sce.getServletContext();
+        context.addServlet("UserServlet", userServlet).addMapping("/users/*");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("\nShutting down ERS web application");
+    }
+}
