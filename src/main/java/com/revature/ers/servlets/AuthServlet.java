e2b8549f44bcbc3a65dfe0d908a2cedcac2f07c9
Pelin Akbiyik <pakbiyik@bu.edu>
2022-06-12 00:56:11 -0400

    updates

---
+++ src/main/java/com/revature/ers/servlets/AuthServlet.java
@@ -0,0 +1,56 @@
+package com.revature.ers.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.ers.dtos.requests.LoginRequest;
+import com.revature.ers.dtos.responses.Principal;
+import com.revature.ers.services.TokenService;
+import com.revature.ers.services.UserService;
+import com.revature.ers.util.annotations.Inject;
+import com.revature.ers.util.custom_exceptions.AuthenticationException;
+import com.revature.ers.util.custom_exceptions.InvalidRequestException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthServlet extends HttpServlet {
+    @Inject
+    private final ObjectMapper mapper;
+    private final UserService userService;
+
+    private final TokenService tokenService;
+
+    public AuthServlet(ObjectMapper mapper, UserService userService, TokenService tokenService) {
+        this.mapper = mapper;
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            LoginRequest request = mapper.readValue(req.getInputStream(), LoginRequest.class);
+            Principal principal = new Principal(userService.login(request));
+
+            /* Stateful session management. */
+            String token = tokenService.generateToken(principal);
+            resp.setHeader("Authorization", token);
+            resp.setContentType("application/json");
+            resp.getWriter().write(mapper.writeValueAsString(principal));
+        } catch (InvalidRequestException e) {
+            resp.setStatus(404);
+        } catch (AuthenticationException e) {
+            resp.setStatus(401);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("<h1>Hello</h1>");
+    }
+}
